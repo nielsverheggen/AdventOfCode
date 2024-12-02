ba5c97a80f6d71713ef8ec389e8e94646088cd39
Niels Verheggen <nielsvh23@gmail.com>
2024-12-02 14:30:15 +0100

    Day 01

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,5 +1,41 @@
 use utils;
 
 fn main() {
-    println!("Solution for Day 1");
+    let input_rows = utils::parse_lines("day01/src/input.txt");
+    let mut col1: Vec<i32> = Vec::new();
+    let mut col2: Vec<i32> = Vec::new();
+
+    for line in input_rows {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        if parts.len() == 2 {
+            let num1: i32 = parts[0]
+                .parse()
+                .expect("Failed to parse number in column 1");
+            let num2: i32 = parts[1]
+                .parse()
+                .expect("Failed to parse number in column 2");
+
+            col1.push(num1);
+            col2.push(num2);
+        } else {
+            panic!("Each row must contain exactly two parts");
+        }
+    }
+    col1.sort();
+    col2.sort();
+
+    let total_distance: i32 = col1
+        .iter()
+        .zip(col2.iter()) 
+        .map(|(a, b)| (a - b).abs())
+        .sum();
+
+    println!("Part1: {}", total_distance);
+
+    let mut similarity = 0;
+    for &score in &col1 {
+        similarity += score * col2.iter().filter(|&&x| x == score).count() as i32;
+    }
+
+    println!("Part2: {}", similarity);
 }
